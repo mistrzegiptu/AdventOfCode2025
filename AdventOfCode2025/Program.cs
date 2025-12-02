1a2cf7641d0fb4106b96a6b043bd323b331fc803
mistrzegiptu <mistrzegiptu@gmail.com>
2025-12-02 01:17:00 +0100

    Finish day1 with one liners :V

--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main()
+    {
+        const int startPosition = 50;
+        const int maxPosition = 100;
+
+        var inputData = File.ReadLines("../../../input.txt");
+
+        int currentPosition = startPosition;
+        int counter = 0;
+        foreach(var line in inputData)
+        {
+            char direction = line[0];
+            int rotations = Convert.ToInt32(line[1..]);
+
+            if (direction == 'R')
+            {
+                counter += (currentPosition + rotations) / 100;
+                currentPosition = (currentPosition + rotations) % maxPosition;
+            }
+            
+            if(direction == 'L')
+            {
+                if(currentPosition == 0)
+                {
+                    counter += rotations / 100;
+                }
+                else if(rotations >= currentPosition)
+                {
+                    counter += 1 + (rotations - currentPosition) / maxPosition;
+                }
+                currentPosition = ((currentPosition - rotations) % maxPosition + maxPosition) % maxPosition; 
+            }
+        }
+
+        Console.WriteLine(counter);
+    }
+}
---
+++ Day1/Program.cs
@@ -0,0 +1,16 @@
+﻿const int startPosition = 50;
+const int maxPosition = 100;
+
+var inputData = File.ReadLines("../../../input.txt");
+
+//Part One
+Console.WriteLine(inputData.Select(line => (direction: line[0], rotations: Convert.ToInt32(line[1..]))).
+    Aggregate((position: startPosition, zeroes: 0), (x, next) => next.direction == 'R' ?
+        ((x.position + next.rotations) % maxPosition, x.zeroes + (x.position == 0 ? 1 : 0)) :
+        ((x.position - next.rotations % maxPosition + maxPosition) % maxPosition, x.zeroes + (x.position == 0 ? 1 : 0))).zeroes);
+
+//Part Two
+Console.WriteLine(inputData.Select(line => (direction: line[0], rotations: Convert.ToInt32(line[1..]))).
+    Aggregate((position: startPosition, zeroes: 0), (x, next) => next.direction == 'R' ?
+        ((x.position + next.rotations) % maxPosition, x.zeroes + ((x.position + next.rotations) / maxPosition)) :
+        (((x.position - next.rotations) % maxPosition + maxPosition) % maxPosition, x.zeroes + (x.position == 0 ? (next.rotations / maxPosition) : (next.rotations >= x.position ? (1 + (next.rotations - x.position) / maxPosition) : 0)))).zeroes);
