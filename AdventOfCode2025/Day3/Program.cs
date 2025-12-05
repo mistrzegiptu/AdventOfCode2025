053ccb3620f5ef860d4d27321c4f86c5a12893f2
mistrzegiptu <mistrzegiptu@gmail.com>
2025-12-05 19:04:57 +0100

    Finished Day3

---
+++ AdventOfCode2025/Day3/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        var inputData = File.ReadAllLines("../../../input.txt").Select(x=>x.ToCharArray().Select(y => Convert.ToInt32(y-'0')).ToList());
+
+        GetMax(inputData, 2);
+        GetMax(inputData, 12);
+    }
+
+    public static void GetMax(IEnumerable<IEnumerable<int>> batteries, int count)
+    {
+        long joltageResult = 0;
+        foreach(List<int> battery in batteries)
+        {
+            var stack = new Stack<int>();
+            int k = battery.Count - count;
+            foreach (var i in battery)
+            {
+                while (stack.Count > 0 && stack.Peek() < i && k > 0)
+                {
+                    stack.Pop();
+                    k--;
+                }
+                stack.Push(i);
+            }
+
+            while(stack.Count > 0 && k-- > 0)
+            {
+                stack.Pop();
+            }
+
+            joltageResult += stack.Reverse().Aggregate(0L, (acc, next) => acc * 10L + next);
+        }
+
+        Console.WriteLine(joltageResult);
+    }
+}
