351e72fb622ca374d401f91acfc5349f0f0874f9
Mistrz Egiptu <mistrzegiptu@gmail.com>
2025-12-31 03:47:57 +0100

    Optimize Day9

--- AdventOfCode2025/Day9/Program.cs
+++ AdventOfCode2025/Day9/Program.cs
@@ -35,60 +35,24 @@
                 break;
         }
 
-        var maxAreaGreen = inputData.SelectMany(x => inputData.Select(y => (x, y))).OrderByDescending(pair => Area(pair.x, pair.y)).First(pair => IsInsideBounds(greenLines, EnumerateRect(pair.Item1, pair.Item2)));
+        var maxAreaGreen = inputData.SelectMany(x => inputData.Select(y => (x, y))).OrderByDescending(pair => Area(pair.x, pair.y)).First(pair => IsInsideBounds(greenLines, pair.Item1, pair.Item2));
 
         Console.WriteLine(Area(maxAreaGreen.x, maxAreaGreen.y));
     }
 
-    private static IEnumerable<(long, long)> EnumerateRect((long, long) a, (long, long) b)
+    private static bool IsInsideBounds(List<((long, long), (long, long))> greenLines, (long, long) rectPointA, (long, long) rectPointB)
     {
-        for (long i = Math.Min(a.Item1, b.Item1); i <= Math.Max(a.Item1, b.Item1); i++)
-            for (long j = Math.Min(a.Item2, b.Item2); j <= Math.Max(a.Item2, b.Item2); j++)
-                yield return (i, j);
-    }
+        var bottomLeft = (x: Math.Min(rectPointA.Item1,  rectPointB.Item1), y: Math.Min(rectPointA.Item2, rectPointB.Item2));
+        var topRight = (x: Math.Max(rectPointA.Item1,  rectPointB.Item1), y: Math.Max(rectPointA.Item2, rectPointB.Item2));
 
-    private static bool IsInsideBounds(List<((long, long), (long, long))> greenLines, IEnumerable<(long, long)> rect)
-    {
-        foreach (var point in rect)
+        foreach (var line in greenLines)
         {
-            int hitCounter = 0;
-            foreach (var line in greenLines)
-            {
-                if (point.Item1 == line.Item1.Item1 && point.Item1 == line.Item2.Item1)
-                {
-                    hitCounter = 1;
-                    break;
-                }
-                if (point.Item2 == line.Item1.Item2 && point.Item2 == line.Item2.Item2)
-                {
-                    hitCounter = 1;
-                    break;
-                }
-                if (line.Item1.Item2 == line.Item2.Item2)
-                {
-                    if (point.Item1 > line.Item1.Item1)
-                    {
-                        long xMin = Math.Min(line.Item1.Item1, line.Item2.Item1);
-                        long xMax = Math.Max(line.Item1.Item1, line.Item2.Item1);
+            (var a, var b) = line;
 
-                        if (point.Item1 >= xMin && point.Item1 <= xMax)
-                            hitCounter++;
-                    }
-                }
-                else if (line.Item1.Item1 == line.Item2.Item1)
-                {
-                    if (point.Item1 > line.Item1.Item1)
-                    {
-                        long yMin = Math.Min(line.Item1.Item2, line.Item2.Item2);
-                        long yMax = Math.Max(line.Item1.Item2, line.Item2.Item2);
+            var lineA = (x: Math.Min(a.Item1, b.Item1), y: Math.Min(a.Item2, b.Item2));
+            var lineB = (x: Math.Max(a.Item1, b.Item1), y: Math.Max(a.Item2, b.Item2));
 
-                        if (point.Item2 >= yMin && point.Item2 <= yMax)
-                            hitCounter++;
-                    }
-                }
-            }
-
-            if (hitCounter % 2 == 0)
+            if(lineA.x < topRight.x && lineB.x > bottomLeft.x && lineA.y < topRight.y && lineB.y > bottomLeft.y)
                 return false;
         }
 
