97989b1f7c7b297c01c22d07823e1c7517eda912
Mistrz Egiptu <mistrzegiptu@gmail.com>
2026-01-04 01:17:57 +0100

    Finished Day10 part 2

--- AdventOfCode2025/Day10/Program.cs
+++ AdventOfCode2025/Day10/Program.cs
@@ -15,40 +15,92 @@ internal class Program
         foreach (var line in inputData)
         {
             ParseLine(line);
-            minClicksToLight += MinClicksRequired();
-            minClicksToJoltage += MinClicksToJoltageRequired();
+            minClicksToLight += ClicksRequired(_targetValue, _buttons).Select(x => long.PopCount(x)).Min();
+            minClicksToJoltage += MinClicksToJoltageRequired(_joltageRequirements, _buttons);
         }
 
         Console.WriteLine(minClicksToLight);
+        Console.WriteLine(minClicksToJoltage);
     }
 
-    private static long MinClicksRequired()
+    private static IEnumerable<long> ClicksRequired(long targetValue, List<long> buttons)
     {
-        int n = _buttons.Count;
-        long minClicks = long.MaxValue;
+        int n = buttons.Count;
 
         for (int i = 0; i < (1 << n); i++)
         {
             long startValue = 0L;
             for (int j = 0; j < n; j++)
             {
-                if ((i & (1 << j)) == 1 << j)
+                if ((i & (1 << j)) != 0)
                 {
-                    startValue ^= _buttons[j];
+                    startValue ^= buttons[j];
                 }
             }
-            if(startValue == _targetValue)
+            if(startValue == targetValue)
             {
-                minClicks = Math.Min(minClicks, long.PopCount(startValue));
+                yield return i;
+            }
+        }
+    }
+
+    private static long MinClicksToJoltageRequired(long[] joltageRequirements, List<long> buttons)
+    {
+        return SearchMinClicks(joltageRequirements, buttons, 0L);
+    }
+
+    private static long SearchMinClicks(long[] joltageRequirements, List<long> buttons, long clicked)
+    {
+        for (int i = 0; i < buttons.Count; i++)
+        {
+            if ((clicked & (1 << i)) != 0)
+            {
+                for(int j = 0; j < joltageRequirements.Length; j++)
+                { 
+                    joltageRequirements[j] -= ((buttons[i] >> j) & 1);
+                    
+                    if (joltageRequirements[j] < 0)
+                        return long.MaxValue;
+                }      
             }
         }
 
-        return minClicks;
+        if (joltageRequirements.All(x => x == 0))
+            return long.PopCount(clicked);
+
+        var factor = 1L;
+        if(joltageRequirements.All(x => x % 2 == 0))
+        {
+            factor = 2L;
+            joltageRequirements = joltageRequirements.Select(x => x / 2).ToArray();
+        }
+
+        var resultClicks = long.MaxValue;
+        foreach (var clicks in ClicksRequired(ConvertToEvenBin(joltageRequirements), buttons))
+        {
+            var result = SearchMinClicks((long[])joltageRequirements.Clone(), buttons, clicks);
+            if (result != long.MaxValue)
+            {
+                result = factor * result + long.PopCount(clicked);
+                resultClicks = Math.Min(resultClicks, result);
+            }
+        }
+
+        return resultClicks;
     }
 
-    private static long MinClicksToJoltageRequired()
+    private static long ConvertToEvenBin(long[] joltageRequirements)
     {
-        return 0;
+        long evenBin = 0L;
+        for (int i = 0; i < joltageRequirements.Length; i++)
+        {
+            if((joltageRequirements[i] & 1) != 0)
+            {
+                evenBin |= 1L << i;
+            }
+        }
+
+        return evenBin;
     }
 
     #region Parsing
